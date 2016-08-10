bcd711ee39dd3fe1ee35dda637e841257d615d67
Nathan <thaonk26@uww.edu>
2016-08-09 21:25:28 -0500

    initial upload

---
+++ virtualReality/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualReality
+{
+    class Game
+    {
+        Player mainPlayer;
+
+        public Game()
+        {
+            Console.WriteLine("Welcome to the Royal World!\n What should we call you by?");
+            this.mainPlayer = new Player(Console.ReadLine());
+        }
+        public void RunProgram()
+        {
+            Console.WriteLine("We warmly welcome you {0}", mainPlayer.seePlayer());
+            startingTown();
+        }
+        public string startingTown()
+        {
+            Console.WriteLine("What town would you like to start at?\n Azure Town, Silver Town or Crimson Town?");
+            mainPlayer.startingTown(Console.ReadLine());
+            switch (mainPlayer.setTown())
+            {
+                case "azure":
+                    Console.WriteLine("nice choice choosing the blue color");
+                    break;
+                case "silver":
+                    Console.WriteLine("thats a great color");
+                    break;
+                case "crimson":
+                    Console.WriteLine("a deep red color like blood");
+                    break;
+                default:
+                    Console.WriteLine("umm....thats not a town");
+                    break;
+            }
+            return mainPlayer.setTown();
+        }
+    }
+}
---
+++ virtualReality/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualReality
+{
+    class Player
+    {
+        string player;
+        string town;
+
+        public Player(string player)
+        {
+            this.player = player;
+        }
+        public string seePlayer()
+        {
+            return this.player;
+        }
+        public void startingTown(string town)
+        {
+            this.town = town;
+        }
+        public string setTown()
+        {
+            return town;
+        }
+    }
+}
